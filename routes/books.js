6c68e6a0050d0cdf612c7e882658e945b67d875d
Torrey Smith <torreypsmith@aol.com>
2018-04-03 08:59:19 -0600

    completed part2 routes

--- routes/books.js
+++ routes/books.js
@@ -11,8 +11,15 @@ const router = express.Router();
 
 router.use(bodyParser.json());
 
-
-
+router.get('/books/:id', (req, res, next) => {
+  const { id } = req.params
+  knex('books')
+    .orderBy('title')
+    .where('id', id)
+    .then(book => {
+      res.json(humps.camelizeKeys(book)[0])
+    })
+})
 
 router.get('/books', (req, res, next) => {
   knex('books')
@@ -21,4 +28,35 @@ router.get('/books', (req, res, next) => {
       res.json(humps.camelizeKeys(book))
     })
 })
+
+router.post('/books', (req, res, next) => {
+      knex('books')
+        .insert(humps.decamelizeKeys(req.body))
+        .insert(req.body['id'] = 9)
+        .then(book => {
+        res.status(200).json(humps.camelizeKeys(req.body))
+    })
+})
+
+router.patch('/books/:id', (req, res, next) => {
+
+    //   knex('books')
+    //     .insert(humps.decamelizeKeys(req.body))
+    //     .insert(req.body['id'] = 1)
+    //     .then(book => {
+    //     res.status(200).json(humps.camelizeKeys(req.body))
+    // })
+
+    req.body['id'] = req.params.id
+    res.status(200).json(req.body)
+})
+
+router.delete('/books/:id', (req, res, next) => {
+  knex('books')
+    .select('author', 'cover_url', 'description', 'genre', 'title')
+    .then(book => {
+      res.json(humps.camelizeKeys(book)[0])
+    })
+})
+
 module.exports = router;
